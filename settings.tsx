d789b0274a70e84187d1b992871eedd3bb84a50e
Blockchainjosh <94154157+Josh0007-sunday@users.noreply.github.com>
2024-03-05 15:30:12 +0100

    Add files via upload

---
+++ settings.tsx
@@ -0,0 +1,14 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://shdw-drive.genesysgo.net/55QiXCJPPiYADZfVimRzi4tscrz918GyaKLr46vVMRgn/0.png";
+
+//website title
+export const headerText = "The Solana Husky Mint";
